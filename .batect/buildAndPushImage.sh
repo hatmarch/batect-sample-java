7c18e7b8a166543480bd02734dcf80e05451e3fc
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-01 02:28:11 +0000

    Update build and push to use the DockerUser in the name of the iamge

--- .batect/buildAndPushImage.sh
+++ .batect/buildAndPushImage.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 TAG=$(git rev-parse HEAD)
 
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build -t charleskorn/batect-sample-java:latest .batect/international-transfers-service
-docker tag charleskorn/batect-sample-java:latest charleskorn/batect-sample-java:$TAG
-docker push charleskorn/batect-sample-java:latest
-docker push charleskorn/batect-sample-java:$TAG
+docker build -t $DOCKER_USER/batect-sample-java:latest .batect/international-transfers-service
+docker tag $DOCKER_USER/batect-sample-java:latest $DOCKER_USER/batect-sample-java:$TAG
+docker push $DOCKER_USER/batect-sample-java:latest
+docker push $DOCKER_USER/batect-sample-java:$TAG
